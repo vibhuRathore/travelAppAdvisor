5cd9d209f8bf26f2064b5f0c1bc7ab9f929bea05
Vibhu Rathore <vibhurathore34@gmail.com>
2021-10-14 23:56:04 +0530

    build: disable incongrous eslint errors causing build fails

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { createRef, useEffect, useState } from 'react';
 import {
   Box,
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef, useState } from 'react';
 import { Grid } from '@mui/material';
 import { useViewportBounds } from '../../contexts/ViewportContext';
